3c9146efc02653f7d659b002cdf7ae4d71a618b9
lingchenxuan <lingchenxuan@eluying.com>
2016-09-27 13:44:20 +0800

    feat: add dd-datepicker

--- src/components/datepicker/calendar.vue
+++ src/components/datepicker/calendar.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <div></div>
+    <div class="dd-datepicker-calendar">
+        <div class="dd-datepicker-header">
+            <i class="dd-datepicker-icon dd-datepicker-icon-prev-year" @click="changeYear(year - 1)"></i>
+            <i class="dd-datepicker-icon dd-datepicker-icon-prev-month" @click="changeMonth(month - 1)"></i>
+            <span class="dd-datepicker-year-month">{{this.year}}年{{this.month + 1}}月</span>
+            <i class="dd-datepicker-icon dd-datepicker-icon-next-month" @click="changeMonth(month + 1)"></i>
+            <i class="dd-datepicker-icon dd-datepicker-icon-next-year" @click="changeYear(year + 1)"></i>            
+        </div>
         <div>
-            <table>
+            <table class="dd-datepicker-table">
                 <thead>
                     <tr>
                         <th v-for="WEEK in WEEKS">{{WEEK}}</th>
@@ -10,38 +16,205 @@
                 </thead>
                 <tbody>
                     <tr v-for="row in rows">
-                        <td v-for="day in row">{{day.getDate()}}</td>
+                        <td v-for="day in row"
+                            >
+                            <div :class="getCellClasses(day)"
+                                @click="selectDate(day)"
+                            >
+                                {{isSameDate(day, new Date()) ? '今' : day.getDate()}}
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
-<style>
+<style  lang="sass" scoped>
+    @import"../../../node_modules/dd-common-css/src/variables";
+    .dd-datepicker-calendar {
+        background: #fff;
+        position: absolute;
+        left: 0;
+        top: 24px;
+        margin-top: 4px;
+        padding: 8px;
+        box-shadow:0px 0px 5px 0px rgba(0,0,0,0.15);
+        border-radius:2px;
+        z-index: 10;
+    }
+    .dd-datepicker-header {
+        margin: 8px 0;
+    }
+    .dd-datepicker-year-month {
+        font-size: $font-size-sm;
+        font-weight: bold;
+        text-align: center;
+        display: inline-block;
+        margin: 0 35px;
+        min-width: 64px;
+    }
+    .dd-datepicker-icon {
+        cursor: pointer;
+        width: 8px;
+        height: 8px;
+        display: inline-block;
+        background: no-repeat center;
+        user-select: none;
 
-</style>
-<script>
-export default {
-    name: 'calendar',
-    data() {
-        return {
-            date: new Date(),
-            WEEKS: ['一', '二', '三', '四', '五', '六', '日']
+        &.dd-datepicker-icon-prev-year {
+            margin-left: 8px;
+            background-image: url('images/double-left-arrow.png');
         }
-    },
-    computed: {
-        rows() {
-            const date = new Date(this.year, this.month, 1);
-            return date.getDay();
+        &.dd-datepicker-icon-prev-month {
+            margin-left: 24px;
+            background-image: url('images/left-arrow.png');
         }
-    },
-    watch: {
-        date(newVal) {
-            if (!this.year) {
-                this.year = newVal.getFullYear();
-                this.month = newVal.getMonth();
+        &.dd-datepicker-icon-next-year {
+            background-image: url('images/double-right-arrow.png');
+
+        }
+        &.dd-datepicker-icon-next-month {
+            margin-right: 24px;
+            background-image: url('images/right-arrow.png');
+        }
+    }
+    .dd-datepicker-table {
+        text-align: center;
+        table-layout: fixed;
+        width: 231px;
+        td, th {
+            width: 33px;
+            height: 35px;
+        }
+        th {
+            color: $gary-dark;
+            font-weight: normal;
+        }
+        tbody td {
+            .dd-datepicker-prev-month,.dd-datepicker-next-month {
+                color: $gary;
+            }
+            .dd-datepicker-date {
+                width: 24px;
+                height: 24px;
+                border-radius: 2px;
+                line-height: 24px;
+                margin: auto;
+                cursor: pointer;
+                transition: background .2s ease-in-out;
+                &:hover {
+                    background: $blue-lighter;
+                }
+            }
+            .dd-datepicker-today {
+                color: $blue;
+            }
+            .dd-datepicker-current {
+                background: $blue;
+                color: #fff;
             }
         }
     }
+</style>
+<script>
+import { getRows, isSameDate } from './utils';
+export default {
+    name: 'calendar',
+
+    props: {
+        defaultValue: [String, Date]
+    },
+
+    data() {
+        return {
+            date: this.defaultValue ? new Date(this.defaultValue) : new Date(),
+            WEEKS: ['一', '二', '三', '四', '五', '六', '日'],
+            year: null,
+            month: null,
+        }
+    },
+
+    computed: {
+        rows() {
+            const date = new Date(this.year, this.month, 1);
+            return getRows(date);
+        }
+    },
+
+    methods: {
+        selectDate(day) {
+            this.date = day;
+            
+        },
+
+        changeYear(year) {
+            this.year = year;
+        },
+
+        changeMonth(month) {
+            if (month === -1) {
+                this.month = 11;
+                this.year  --;
+            } else if (month === 12) {
+                this.month = 0;
+                this.year  ++;
+            } else {
+                this.month = month;
+            }
+        },
+
+        getCellClasses(cell) {
+            const classes = ['dd-datepicker-date'];
+            if (cell.getFullYear() < this.year || cell.getMonth() < this.month) {
+                classes.push('dd-datepicker-prev-month');
+            }
+
+            if (cell.getFullYear() > this.year || cell.getMonth() > this.month) {
+                classes.push('dd-datepicker-next-month');
+            }
+
+            if (cell.getMonth() === this.month) {
+                classes.push('dd-datepicker-normal');
+            }
+
+            if (isSameDate(cell, new Date())) {
+                classes.push('dd-datepicker-today');
+            }
+
+            if (isSameDate(cell, this.date)) {
+                classes.push('dd-datepicker-current');
+            }
+
+            return classes.join(' ');
+        },
+
+        isSameDate(date1, date2) {
+            return isSameDate(date1, date2);
+        }
+    },
+
+    watch: {
+        date(newVal) {
+            this.year = newVal.getFullYear();
+            this.month = newVal.getMonth();
+            this.$emit('selectDate', this.date);
+        },
+
+        defaultValue(newVal) {
+            this.date = new Date(newVal);
+        }
+    },
+
+    created() {
+        if (this.date && !this.year) {
+            this.year = this.date.getFullYear();
+            this.month = this.date.getMonth();
+        }
+
+        if (this.defaultValue) {
+            this.$emit('selectDate', this.date);
+        }
+    },
 }
 </script>
--- src/components/datepicker/index.vue
+++ src/components/datepicker/index.vue
@@ -1,27 +1,118 @@
 <template>
-    <span>
-        <span>
-            <input type="text">
-            <i></i>
+    <span class="dd-datepicker" v-clickoutside="hideCalendar">
+        <span class="dd-datepicker-input">
+            <input
+                :placeholder="placeholder"
+                :disabled="disabled"
+                class="dd-input"
+                type="text"
+                @focus="handleFocus"
+                v-model.lazy="dateStr" />
+            <i @click="handleClickCalendarIcon" class="dd-datepicker-calendar-icon" :class="{disabled: disabled}"></i>
         </span>
-        <calendar />
+        <calendar v-show="calendarVisible" v-on:selectDate="handleSelectDate" :default-value="value" />
     </span>
 </template>
-<style>
-
+<style lang="sass">
+    .dd-datepicker {
+        position: relative;
+        display: inline-block;
+    }
+    .dd-datepicker-input {
+        position: relative;
+    }
+    .dd-datepicker-calendar-icon {
+        background: url('images/calendar.png') center no-repeat;
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        position: absolute;
+        right: 8px;
+        top: 1px;
+        cursor: pointer;
+        &.disabled {
+            cursor: not-allowed;
+        }
+    }
 </style>
 <script>
     import calendar from './calendar.vue';
+    import Clickoutside from '../../utils/clickoutside'
+    import { formatDate } from './utils';
     export default {
         name: 'DdDatePicker',
+
         props: {
+            disabled: Boolean,
             defaultValue: [String, Date],
-            format: String,
             placeholder: String,
             disabledDate: Function,
+            value: {},
         },
+
+        data() {
+            return {
+                calendarVisible: false,
+                date: null,
+                dateStr: null,
+            }
+        },
+
+        watch: {
+            date(newVal) {
+                this.dateStr = formatDate(newVal);
+                this.$emit('input', this.dateStr);
+            }
+        },
+
+        computed: {
+
+        },
+
+        methods: {
+            showCalendar() {
+                this.calendarVisible = true;
+            },
+
+            hideCalendar() {
+                this.calendarVisible = false;
+            },
+            handleFocus() {
+                !this.calendarVisible && this.showCalendar();
+                this.$emit('focus', this);
+            },
+
+            handleBlur() {
+                this.$emit('blur', this);
+            },
+
+            handleSelectDate(date) {
+                this.date = date;
+                this.hideCalendar();
+            },
+
+            toggleCalendar() {
+                if (this.calendarVisible) {
+                    this.hideCalendar();
+                } else {
+                    this.showCalendar();
+                }
+            },
+
+            handleClickCalendarIcon() {
+                if (this.disabled) {
+                    return
+                }
+                this.toggleCalendar();
+            }
+        },
+
         components: {
             calendar
+        },
+
+        directives: {
+            Clickoutside
         }
     }
 </script>
