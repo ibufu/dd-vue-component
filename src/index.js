4e3ff83a42acc0dc49759718ae91897104e09582
lingchenxuan <lingchenxuan@eluying.com>
2016-10-05 21:12:06 +0800

    fix: add export components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import DdDatepicker from './components/datepicker';
 import DdPagination from './components/pagination';
 import DdSelect from './components/select';
+import DdOption from './components/select/option';
 import DdDropdown from './components/dropdown';
+import DdDropdownItem from './components/dropdown/item';
 
 module.export = {
     DdDatepicker,
     DdPagination,
     DdSelect,
+    DdOption,
     DdDropdown,
+    DdDropdownItem,
 };
